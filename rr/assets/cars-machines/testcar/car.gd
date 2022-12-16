81f4d1137e47dcf23ae87151494931c16e1993f7
schultzietheR <112105355+schultzietheR@users.noreply.github.com>
2022-12-15 22:05:21 -0700

    Update car.gd

--- rr/assets/cars-machines/testcar/car.gd
+++ rr/assets/cars-machines/testcar/car.gd
@@ -1,15 +1,108 @@
+# I smashed up some code together from Bastiaan Olij (https://www.youtube.com/c/BastiaanOlij)
+# and Jus'ko (https://www.youtube.com/channel/UCI2wrI4NV1zHtZhVMmnj-nQ).
+
+# Problems so far:
+
+# Car doesn't move
+# No sound
+# No smoke from drifting/oversteer
+
 extends VehicleBody
 
-export var torque = 250
-export var steer_angle = .5
-export var max_wheel_rpm = 200
-export var max_torque = 500
+	###########
+
+export var horsePower = 200
+
+	###########
+
+export(Array, float) var gearRatios = [3.00, 2.50, 2.00, 1.50, 1.00]
+export var finalDrive = 3.00
+export var reverseRatio = -2.50
+var currentGear = 0
+
+export var autotrnsmis = false
+
+func transmission():
+	if Input.is_action_just_pressed("shiftup"):
+		currentGear += 1
+	elif Input.is_action_just_pressed("shiftdown"):
+		currentGear -= 1
+
+	for i in (gearRatios):
+		i += currentGear
+		horsePower *= ((gearRatios[currentGear])/finalDrive)
+
+	if currentGear < 0:
+		horsePower = horsePower * reverseRatio
+	return horsePower
+
+	###########
+
+export var idleRPM = 800
+export var RPMredline = 7000
+export var peakRPM = 10000
+var engineRPM = idleRPM
+
+func getRPM():
+	if Input.is_action_just_pressed("gas"):
+		engineRPM +=10
+		horsePower = horsePower * (engineRPM/1000)
+	elif Input.is_action_just_released("gas"):
+		engineRPM -=10
+		horsePower = horsePower * (engineRPM/1000)
+	elif Input.is_action_just_released("gas") and engineRPM <= idleRPM:
+		engineRPM = idleRPM
+
+	###########
+
+
+export var turbocharger = false
+export var p_s_i = false
+export var EV_Motor = false
+export var EV_Power = false
+
+func turbo():
+	var turbopower = Input.get_action_strength("gas")
+	if turbocharger:
+		horsePower = horsePower + ((p_s_i * turbopower)/2)
+		if EV_Motor:
+			horsePower = horsePower + (EV_Power + ((p_s_i * turbopower)/2))
+	elif EV_Motor:
+		horsePower = horsePower + EV_Power
+	else:
+		horsePower = horsePower
+	return horsePower
+
+	###########
+
+export var steerAngle = deg2rad(25)
+var steerSpeed = 3
+
+export var brakePower = 40
+var brakeSpeed = 40
+
+	###########
+
+var throtMult = 1
+var steerMult = 1
+var brakeMult = 1
 
 func _physics_process(delta):
-	steering = lerp(steering, Input.get_axis("steerright","steerleft") * steer_angle, 5*delta)
-	var acceleration = Input.get_axis("brake","accelerate")
-	var wheel_rpm = abs($rear_left_wheel.get_rpm())
-	$rear_left_wheel.engine_force = acceleration * max_torque * (1-wheel_rpm/max_wheel_rpm)
-	wheel_rpm = abs($rear_right_wheel.get_rpm())
-	$rear_right_wheel.engine_force = acceleration * max_torque * (1-wheel_rpm/max_wheel_rpm)
-	engine_force = acceleration * torque
+	
+	###########
+	
+	var throtInput = Input.get_action_strength("gas")
+	var carpower = (engine_force * horsePower * throtInput * delta)
+	engine_force = carpower
+
+	###########
+
+	var steerInput = steerMult * (Input.get_action_strength("left")-Input.get_action_strength("right"))
+	steering = lerp_angle(steering,steerInput*steerAngle,steerSpeed*delta)
+
+	###########
+
+	var brakeInput = brakeMult * Input.get_action_strength("brake")
+	brake = lerp(brake,brakeInput*brakePower,brakeSpeed*delta)
+
+	###########
